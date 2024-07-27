bba84c64f5c0d7ec21ab1d74c0d4c3d7a718aaec
Wing Lee <44917547+wingleeio@users.noreply.github.com>
2024-07-27 14:53:33 -0700

    Update env.ts

--- apps/bot/src/lib/env.ts
+++ apps/bot/src/lib/env.ts
@@ -1,36 +1,38 @@
 import { join } from "path";
 
-console.log("Configuring environment...");
+if (process.env.NODE_ENV !== "production") {
+    console.log("Configuring environment...");
 
-const TSConfig = JSON.stringify(
-    {
-        compilerOptions: {
-            rootDirs: [".."],
-            paths: {
-                "$*": ["../src/*"],
-                $env: ["./env.ts"],
+    const TSConfig = JSON.stringify(
+        {
+            compilerOptions: {
+                rootDirs: [".."],
+                paths: {
+                    "$*": ["../src/*"],
+                    $env: ["./env.ts"],
+                },
+                verbatimModuleSyntax: true,
+                isolatedModules: true,
+                lib: ["esnext"],
+                moduleResolution: "bundler",
+                module: "esnext",
+                noEmit: true,
+                target: "esnext",
             },
-            verbatimModuleSyntax: true,
-            isolatedModules: true,
-            lib: ["esnext"],
-            moduleResolution: "bundler",
-            module: "esnext",
-            noEmit: true,
-            target: "esnext",
+            include: ["../src/**/*.js", "../src/**/*.ts"],
+            exclude: ["../node_modules/**"],
         },
-        include: ["../src/**/*.js", "../src/**/*.ts"],
-        exclude: ["../node_modules/**"],
-    },
-    null,
-    4
-);
+        null,
+        4
+    );
 
-await Bun.write(join(".project", "tsconfig.json"), TSConfig);
+    await Bun.write(join(".project", "tsconfig.json"), TSConfig);
 
-const variables = Object.keys(process.env);
-const contents = variables.map((variable) => `export const ${variable} = process.env.${variable}!;`).join("\n");
-const env = "//@ts-nocheck\n" + contents;
+    const variables = Object.keys(process.env);
+    const contents = variables.map((variable) => `export const ${variable} = process.env.${variable}!;`).join("\n");
+    const env = "//@ts-nocheck\n" + contents;
 
-await Bun.write(join(".project", "env.ts"), env);
+    await Bun.write(join(".project", "env.ts"), env);
 
-console.log("Done!");
+    console.log("Done!");
+}
